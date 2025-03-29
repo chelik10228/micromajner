d2f82ad3038421bbd5e091627e1b786ea348f9ac
chelik10228 <dxxeas@gmail.com>
2025-03-29 08:52:24 +0100

    Update micromajner.py

--- micromajner.py
+++ micromajner.py
@@ -1,10 +1,5 @@
-import time;
-import sys;
-sys.set_int_max_str_digits(0);
-
-money = 1;
+money = .1;
 
 while (True):
-    money *= 2.;
+    money *= .2;
     print("\033[92m", money, "\033[0m");
-    time.sleep(1/20);
